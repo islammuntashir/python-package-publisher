d15e263d4bf11c0eff779184513962f85803b520
Muntashir Islam <muntashir.islam@hishab.co>
2021-07-25 21:08:07 -0600

    changed

--- publisher.sh
+++ publisher.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install twine wheel
 python setup.py sdist bdist_wheel
+export token=$(aws codeartifact get-authorization-token --domain hishab --domain-owner 140370042521 --query authorizationToken --output text)
 twine upload --repository codeartifact dist/* -u aws -p $token
